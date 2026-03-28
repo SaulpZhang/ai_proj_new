f5d33947e26565ec953cc8b208d09238df3b55ba
saulp <saulp_zhang@foxmail.com>
2026-03-28 18:01:31 +0800

    record

--- record_wandb.py
+++ record_wandb.py
@@ -1,14 +1,19 @@
 import wandb
 
+run = None
+
 # Start a new wandb run to track this script.
-run = wandb.init(
-    # Set the wandb entity where your project will be logged (generally your team name).
-    entity="qijizhang5-city-university-of-hong-kong",
-    # Set the wandb project where this run will be logged.
-    project="funsearch",
-    # Track hyperparameters and run metadata.
-    name="funsearch_new",
-)
+
 
 def get_wandb_run():
+    global run
+    if run is None:
+        run = wandb.init(
+            # Set the wandb entity where your project will be logged (generally your team name).
+            entity="qijizhang5-city-university-of-hong-kong",
+            # Set the wandb project where this run will be logged.
+            project="funsearch",
+            # Track hyperparameters and run metadata.
+            name="funsearch_ori",
+        )
     return run
