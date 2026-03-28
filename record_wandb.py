44761e1084ca02b05e8ebf13132789ab888de166
saulp <saulp_zhang@foxmail.com>
2026-03-28 18:34:21 +0800

    debug

--- record_wandb.py
+++ record_wandb.py
@@ -24,7 +24,7 @@ def get_wandb_run():
         # Set the wandb project where this run will be logged.
         project="funsearch",
         # Track hyperparameters and run metadata.
-        name="funsearch_ori",
+        name="funsearch_new",
     )
     return run
 
